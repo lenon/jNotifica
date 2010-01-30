580933acf3f9dadde0d573732032d7e06b75d3c3
Lenon Marcel <lenon.marcel@gmail.com>
2010-01-30 00:22:12 -0200

    v3 Alpha - Initial release

--- js/examples.js
+++ js/examples.js
@@ -1,38 +1,86 @@
 jQuery(document).ready(function(){
-	$('#ex1').click(function(){
-		$.jnotifica();
-	});
-	$('#ex2').click(function(){
-		$.jnotifica('Olha só, funciona! \\o/');
-	});
-	$('#ex3').click(function(){
-		$.jnotifica('Fechando em 4, 3, 2... :P',{},4000);
-	});
-	$('#ex4').click(function(){
-		$.jnotifica('Enquanto você não clicar, a mensagem vai continuar aqui',{},0);
-	});
-	$('#ex5').click(function(){
-		$.jnotifica('Opa',{effect: 'fade' });
-	});
-	$('#ex6').click(function(){
-		$.jnotifica('Prefiro com efeitos =p',{effect: 'none' });
-	});
-	$('#ex7').click(function(){
-		$.jnotifica('Clique para fechar ;-)',{closeButton: false });
-	});
-	$('#ex8').click(function(){
-		$.jnotifica('DANGER! DANGER! DANGER! DANGER! #lol',{css : { background: 'red' } });
-	});
-	$('#ex9').click(function(){
-		$.jnotifica('Salvem as baleias.',{css : { background: 'green', fontSize : '18px', textAlign : 'center' } });
-	});
-	$('#ex10').click(function(){
-		$.jnotifica('Yeahh.',{css : { opacity: 1, textAlign : 'center' } });
-	});
-	$('#ex11').click(function(){
-		$.jnotifica('Aguarde o timeout :)',{closeButton : false, closeClick: false}, 5000);
-	});
-	$('#ex12').click(function(){
-		$.jnotifica('Buuu!',{callback : function(){ alert('Fechou :P')}}, 5000);
-	});
+  $('#example_1').click(function(){
+    $.jnotifica('Hello from Brazil!');
+  });
+  $('#example_2').click(function(){
+    $.jnotifica('Hello from Brazil!',{
+      timeout : 10000
+    });
+  });
+  $('#example_3').click(function(){
+    $.jnotifica('Hello from Brazil!',{
+      background: 'red'
+    });
+  });
+  $('#example_4').click(function(){
+    $.jnotifica('Hello from Brazil!',{
+      background: 'red',
+      opacity   : 1
+    });
+  });
+  $('#example_5').click(function(){
+    $.jnotifica('Hello from Brazil!',{
+      background: 'green',
+      effect: 'fade'
+    });
+  });
+  $('#example_6').click(function(){
+    $.jnotifica('Hello from Brazil!',{
+      background: 'purple',
+      margin: 10
+    });
+  });
+  $('#example_7').click(function(){
+    $.jnotifica('Hello from Brazil!',{
+      margin: 10,
+      width: 400,
+      effect: 'fade',
+      padding: 10,
+      msgCss:{
+        textAlign: 'center'
+      }
+    });
+  });
+  $('#example_8').click(function(){
+    $.jnotifica('Hello from Brazil!',{
+      close: false
+    });
+  });
+  $('#example_9').click(function(){
+    $.jnotifica('Hello from Brazil!',{
+      cursor:'default',
+      clickNClose: false
+    });
+  });
+  $('#example_10').click(function(){
+    $.jnotifica('Aguarde o timeout ;-)',{
+      cursor:'default',
+      clickNClose: false,
+      close: false
+    });
+  });
+  $('#example_11').click(function(){
+    $.jnotifica('Clique para fechar',{
+      timeout: 0
+    });
+  });
+  $('#example_12').click(function(){
+    $.jnotifica('Clique para fechar',{
+      width: 300,
+      align: 'left'
+    });
+  });
+  $('#example_13').click(function(){
+    $.jnotifica('Clique para fechar',{
+      position: 'bottom'
+    });
+  });
+  $('#example_14').click(function(){
+    $.jnotifica('Clique para fechar',{
+      width: 400,
+      align: 'right',
+      margin: 10,
+      position: 'bottom'
+    });
+  });
 });
